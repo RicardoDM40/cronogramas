48355c62fcf4c0a1e66b14938a18e8064c94334d
Ricardo D Melo <ricardodiasdemelo@gmail.com>
2023-05-31 09:17:31 -0300

    checkpoint 31-05

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import { AppDataSource } from "./src/databases/data-source"
+import { AppDataSource } from "./src/databases/connections/data-source"
+AppDataSource.initialize()
 
-const express = require("express") 
-
+const express = require("express")
 const app = express()
-
 app.use(express.json())
 
 app.get('/', (request, response) => {
@@ -13,5 +12,3 @@ app.get('/', (request, response) => {
 app.listen(3333, () =>
     console.log("O server esta ON na porta 3333.")
 )
-
-AppDataSource.initialize()
