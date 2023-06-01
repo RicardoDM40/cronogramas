9142b792085362f52a8083686b154242b9bdaedc
Ricardo D Melo <ricardodiasdemelo@gmail.com>
2023-06-01 11:47:20 -0300

    classes service Ricardo

--- src/services/serviceTurma.ts
+++ src/services/serviceTurma.ts
@@ -3,12 +3,65 @@ import { AppDataSource } from "../databases/connections/data-source"
 
 const cursor = AppDataSource.getRepository(Turma)
 
-export class CreateCursoService {}
+type newTurmaRequest = {
+    fk_curso: string
+    data_inicio: Date
+    data_fim: Date
+    horas_aula_dia: Number
+}
 
-export class ReadAllCursoService {}
+type findOneTurmaRequest = {
+    id_turma: string
+  }
 
-export class ReadOneCursoService {}
+export class CreateCursoService {
+    async execute({
+        fk_curso,
+        data_inicio,
+        data_fim,
+        horas_aula_dia,
+    }: newTurmaRequest): Promise< Turma | Error > {
+        if (await cursor.findOne({where:{fk_curso}})){
+            return new Error("turma já existe!")
+        }
+        const turma = cursor.create({
+            fk_curso,
+            data_inicio,
+            data_fim,
+            horas_aula_dia,
+        }) 
+        
+        await cursor.save(turma)
+        return turma
+    }
+}
 
-export class UpdateCursoService {}
+export class ReadAllTurmaService {
+    async execute() {
+    const turmas = await cursor.find()
+    return turmas
+    }
+}
 
-export class DeleteCursoService {}
+export class ReadOneTurmaService {
+    async execute({ id_turma }: findOneTurmaRequest) {
+        const turma = await cursor.findOne({ where: { id_turma } })
+        if (!turma) {
+          return new Error("Turma não encontrada!")
+        }
+        return turma
+      }
+}
+
+export class UpdateTurmaService {}
+
+export class DeleteturmaService {
+    async execute({ id_turma }: findOneTurmaRequest) {
+        const turma = await cursor.findOne({ where: { id_turma } })
+        if (!turma) {
+          return new Error("Turma não encontrada!")
+        }
+        await cursor.delete(turma.id_turma)
+        return turma
+      }
+}
--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,14 +1,68 @@
-import Curso from "../databases/models/curso"
+import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
+import { type } from "os"
 
-const cursor = AppDataSource.getRepository(Curso)
+const cursor = AppDataSource.getRepository(Unidade)
 
-export class CreateCursoService {}
+type newUnidadeRequest = {
+    fk_curso: string
+    descricao_unidade: string
+    carga_horaria_unidade: number
+    ordem: number
+}
 
-export class ReadAllCursoService {}
+type findOneUnidadeRequest = {
+    id_unidade: string
+  }
 
-export class ReadOneCursoService {}
+export class CreateUnidadeService {
+    async execute({
+        fk_curso,
+        descricao_unidade,
+        carga_horaria_unidade,
+        ordem,
+    }: newUnidadeRequest): Promise < Unidade | Error > {
+        if (await cursor.findOne({where:{descricao_unidade}})){
+            return new Error("turma já existe!")
+        }
+        const unidade = cursor.create({
+            fk_curso,
+            descricao_unidade,
+            carga_horaria_unidade,
+            ordem,
+        }) 
+        
+        await cursor.save(unidade)
+        return unidade
+    }
+}
+
+export class ReadAllUnidadeService {
+    async execute() {
+        const unidades = await cursor.find()
+        return unidades
+      }
+}
+
+export class ReadOneUnidadeService {
+    async execute({ id_unidade }: findOneUnidadeRequest) {
+        const unidade = await cursor.findOne({ where: { id_unidade } })
+        if (!unidade) {
+          return new Error("Unidade não encontrada!")
+        }
+        return unidade
+      }
+}
 
 export class UpdateCursoService {}
 
-export class DeleteCursoService {}
+export class DeleteUnidadeService {
+    async execute({ id_unidade }: findOneUnidadeRequest) {
+        const unidade = await cursor.findOne({ where: { id_unidade } })
+        if (!unidade) {
+          return new Error("Unidade não encontrada!")
+        }
+        await cursor.delete(unidade)
+        return unidade
+      }
+}
